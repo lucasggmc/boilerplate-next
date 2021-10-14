817ce47ad9f065e18f182a5ccb3cd019f737b5d8
lucasggmc <lucasggmc@gmail.com>
2021-10-13 21:54:49 -0300

    configurado PWA

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,6 +10,9 @@ function App({ Component, pageProps }: AppProps) {
         <title>React avançado - Boilerplate</title>
         <link rel="shortcut icon" href="/img/icon-515.png" />
         <link rel="apple-touch-icon" href="/img/icon-515.png" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#06092B" />
+
         <meta
           name="description"
           content="A simple project starter to work with TypeScript, React, NtextJS and Styled Components"
